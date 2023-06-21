a15d729bfe08e26899d05d66a8f95909cc4c23c4
Chaymae-aben <chaymaebennani0@gmail.com>
2023-06-21 16:23:05 -0700

    a

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,27 +7,24 @@
  */
 int main(void)
 {
-    int i;
-    unsigned long int j, k, next;
+	int i;
+	unsigned long int j, k, next;
 
-    j = 1;
-    k = 2;
+	j = 1;
+	k = 2;
 
-    printf("%lu, %lu, ", j, k);  // Print the first two terms
+	printf("%lu, %lu", j, k); /* Print the first two terms */
 
-    for (i = 1; i <= 48; ++i)  // Print the next 48 terms
-    {
-        next = j + k;
-        printf("%lu", next);
+	for (i = 1; i <= 48; ++i) /* Print the next 48 terms */
+	{
+		next = j + k;
+		printf(", %lu", next);
 
-        if (i != 48)  // Add a comma and space for all terms except the last one
-            printf(", ");
+		j = k;
+		k = next;
+	}
 
-        j = k;
-        k = next;
-    }
+	printf("\n");
 
-    printf("\n");
-
-    return (0);
+	return (0);
 }
